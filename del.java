35084f70275b4e059f479a66d952c06a383c07f1
DESKTOP-T526RFM\lenovo <akshatjaiswal521@gmail.com>
2021-12-20 16:03:13 +0530

    initial files

---
+++ del.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+class del{
+ static int count,totalcount;
+ public static void main(String args[]){
+  File file= new File("D:\\Java3");
+  recurse(file);
+  
+  System.out.println(totalcount+" directories found, "+count+" empty directories deleted.");
+ }
+
+ public static void recurse(File file){
+  totalcount++;
+
+  if(file.isDirectory())
+  {
+   File[] filelist=file.listFiles();
+
+   if(filelist.length==0)
+   {
+    file.delete();
+    count++;
+    return;
+   }
+   
+   for(File newfile:filelist)
+   recurse(newfile);
+
+   return;
+  }
+ }
+}
